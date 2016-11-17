9674fe6fa56c539d3cb1dfb7fbebd0e842c65c65
Alyssa Biot <alyssa.biot@gmail.com>
2016-11-17 12:27:14 +0100

    mister cocktail no css2

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :cocktails, only: [ :index, :show, :new, :create ] do
     resources :doses, only: [ :new, :create, :destroy ]
   end
-
+  root 'cocktails#index'
 end
