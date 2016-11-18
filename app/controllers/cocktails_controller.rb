88c12e57cbd50d953e6f06c4cfd3dd03f21aa1ff
Alyssa Biot <alyssa.biot@gmail.com>
2016-11-18 17:19:53 +0100

    modal for new cocktail bug fix

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -18,7 +18,7 @@ class CocktailsController < ApplicationController
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      redirect_to :back
     end
   end
 
