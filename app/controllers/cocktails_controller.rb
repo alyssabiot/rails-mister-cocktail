1fc718a26e5a49e1858bc650ea21729decac9678
Alyssa Biot <alyssa.biot@gmail.com>
2016-11-18 10:55:59 +0100

    figaro install - protect secret data

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -24,7 +24,7 @@ class CocktailsController < ApplicationController
 
   private
   def cocktail_params
-    params.require(:cocktail).permit(:name, :url)
+    params.require(:cocktail).permit(:name)
   end
 end
 
--- db/migrate/20161117161603_add_url_to_cocktails.rb
+++
@@ -1,5 +0,0 @@
-class AddUrlToCocktails < ActiveRecord::Migration[5.0]
-  def change
-    add_column :cocktails, :url, :string
-  end
-end
